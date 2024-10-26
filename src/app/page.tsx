1d0de676b37689e257493750eec4379a561f7675
JiyeonMin <ichbinmin2@gmail.com>
2024-10-26 22:36:05 +0900

    feat : 메인 페이지 세팅

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
-    <div className='w-full h-full'>
+    <section className='mx-auto mt-12 w-full max-w-[950px] px-4'>
       <a className='caret-amber-300' href='/blog'>
-        blog 이동
+        main
       </a>
-    </div>
+    </section>
   );
 }
