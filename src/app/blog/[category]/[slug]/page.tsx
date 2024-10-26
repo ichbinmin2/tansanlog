30717d3ae29d20ad9878e29fc49868dfe5a8141b
JiyeonMin <ichbinmin2@gmail.com>
2024-10-26 22:46:03 +0900

    feat : 상세 페이지 세팅 완료

--- src/app/blog/[category]/[slug]/page.tsx
+++ src/app/blog/[category]/[slug]/page.tsx
@@ -1,5 +1,4 @@
 import { Metadata } from "next";
-
 import { baseDomain } from "@/config/const";
 import {
   getPostDetail,
@@ -7,6 +6,11 @@ import {
   parsePostAbstract,
   parseToc,
 } from "@/lib/post";
+import { PostHeader } from "@/components/post_detail/postHeader";
+import TableOfContentTop from "@/components/post_detail/tableTop";
+import TableOfContent from "@/components/post_detail/tableOfContent";
+import { PostBody } from "@/components/post_detail/postBody";
+import Giscus from "@/components/post_detail/giscus";
 
 type Props = {
   params: { category: string; slug: string };
@@ -31,7 +35,7 @@ export async function generateMetadata({
       title,
       description: post.desc,
       type: "article",
-      publishedTime: post.date.toISOString(),
+      publishedTime: post.date?.toISOString(),
       url: `${baseDomain}${post.url}`,
       images: [imageURL],
     },
@@ -56,7 +60,16 @@ const PostDetail = async ({ params: { category, slug } }: Props) => {
   const toc = parseToc(post.content);
 
   return (
-    <div className='prose mx-auto w-full max-w-[750px] px-5 dark:prose-invert sm:px-6'></div>
+    <div className='prose mx-auto w-full max-w-[750px] px-5 dark:prose-invert sm:px-6'>
+      <PostHeader post={post} />
+      <TableOfContentTop toc={toc} />
+      <article className='relative'>
+        <TableOfContent toc={toc} />
+        <PostBody post={post} />
+      </article>
+      <hr />
+      <Giscus />
+    </div>
   );
 };
 
