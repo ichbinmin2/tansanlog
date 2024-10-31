741472c193c363c39420cef2a9950e9eebe02a28
JiyeonMin <ichbinmin2@gmail.com>
2024-11-01 00:54:27 +0900

    feat : three 3D 인터렉티브 키 비주얼 적용 테스트1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Graphic } from "@/components/main/graphic/graphic";
+
 export default function Home() {
   return (
-    <section className='mx-auto mt-12 w-full max-w-[950px] px-4'>
-      <a className='caret-amber-300' href='/blog'>
-        main
-      </a>
+    <section className='mx-auto w-[100vw] h-[100vh] px-4'>
+      {/* <a className='caret-amber-300' href='/blog'> */}
+      <Graphic />
+      {/* </a> */}
     </section>
   );
 }
